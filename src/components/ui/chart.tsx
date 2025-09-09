d64b4fb1a8abbd521e62764fb44daaf9ce7482ae
Kishan Kumar <kk5005575@gmail.com>
2025-09-09 00:48:07 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -43,8 +43,7 @@ const ChartContainer = React.forwardRef<
     >["children"];
   }
 >(({ id, className, children, config, ...props }, ref) => {
-  const uniqueId = React.useId();
-  const chartId = `chart-${id || uniqueId.replace(/:/g, "")}`;
+  const chartId = `chart-${id || Math.random().toString(36).substring(7)}`;
 
   return (
     <ChartContext.Provider value={{ config }}>
